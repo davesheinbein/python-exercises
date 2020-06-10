3c8d257633ee91822891b256a3bb48eb660d9230
David Sheinbein <davidsheinbein@Davids-MacBook-Pro.local>
2020-06-10 16:56:50 -0700

    update

--- exercises/exercise-6.py
+++ exercises/exercise-6.py
@@ -19,3 +19,8 @@
 #
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
+
+month = input('Enter the month of the year (Jan - Dec):')
+day = input('Enter the day (1-31) of the month:')
+
+
